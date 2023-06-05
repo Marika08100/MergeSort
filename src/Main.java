cd2e46c90af3f5d13025a10f50b555ab38c62213
Marika08100 <132154696+Marika08100@users.noreply.github.com>
2023-06-05 20:58:14 +0300

    mergeSort method rdy for use

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,34 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println(isSorted(new int[]{-6,10,-88,6,0}));
+        System.out.println(isSorted(new int[]{-6, 10, -88, 6, 0}));
 
     }
-    public static boolean isSorted(int[] array){
-        for (int i = 0; i < array.length-1; i++) {
-            if(array[i] > array[i+1]){
+
+    public static boolean isSorted(int[] array) {
+        for (int i = 0; i < array.length - 1; i++) {
+            if (array[i] > array[i + 1]) {
                 return false;
             }
 
         }
         return true;
     }
-    public static int[] mergedArray(int[] leftArray, int[] rightArray ){
+
+    public static int[] merge(int[] leftArray, int[] rightArray) {
+
         return null;
     }
-    public static int[] mergeSort(int[] array){
-        return null;
+
+    public static int[] mergeSort(int[] array) {
+        if (isSorted(array)) {
+            return array;
+        }
+        int[] left = Arrays.copyOfRange(array, 0, array.length / 2);
+        int[] right = Arrays.copyOfRange(array, array.length / 2, array.length);
+        left = mergeSort(left);
+        right = mergeSort(right);
+        return merge(left, right);
     }
 }
