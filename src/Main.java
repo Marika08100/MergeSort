4caa3c59e86a77b620e97ddc05e94896d1e2080d
Marika08100 <132154696+Marika08100@users.noreply.github.com>
2023-06-05 20:36:47 +0300

    add isSorted method

--- src/Main.java
+++ src/Main.java
@@ -3,4 +3,14 @@ public class Main {
         System.out.println("Merge Sort");
 
     }
+    public static boolean isSorted(int[] array){
+        for (int i = 0; i < array.length-1; i++) {
+            if(array[i] > array[i+1]){
+                return false;
+            }
+
+
+        }
+        return true;
+    }
 }
